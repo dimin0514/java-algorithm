9587fd7e2bef9d92403d25ef3efa17a026a6a241
lovepower <lovepower01@naver.com>
2019-08-14 17:59:27 +0900

    190814 18:00

--- homework02/Join.java
+++ homework02/Join.java
@@ -1,9 +1,10 @@
 package homework02;
 
-public class HowMuch {
-	
+public class Join {
+
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- homework02/Ranking.java
+++ homework02/Ranking.java
@@ -1,9 +1,10 @@
 package homework02;
 
-public class HowMuch {
-	
+public class Ranking {
+
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }

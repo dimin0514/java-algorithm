ae12271bd735c8032ca0ff82bfabae47dd7f428b
lovepower <lovepower01@naver.com>
2019-08-20 11:30:45 +0900

    190820 11:30

--- homework03/Engine.java
+++ homework03/Engine.java
@@ -9,7 +9,7 @@ public class Engine {
 		Student student = new Student();
 		
 		while(true) {
-			System.out.println("0.종료 1.사칙연산 2.BMI 3.HowMuch 4.Join 5.LeapYear 6.MonthEndDay 7.PassOrFail 8.Ranking 9.ReportCard 10.ScaoreCalc 11.Tax 12.TimeCalc");
+			System.out.println("0.종료 1.사칙연산 2.BMI 3.HowMuch 4.Join 5.LeapYear 6.MonthEndDay 7.PassOrFail 8.Ranking 9.ScaoreCalc 10.Tax 11.TimeCalc");
 			switch(scanner.nextInt()) {
 			case 0 : ; return;
 			case 1:
@@ -99,10 +99,85 @@ public class Engine {
 				System.out.println(result);
 				
 			break;
+			case 7:
+				String[] subjects = {"국어","영어","수학"};
+				int[] scores = new int[3];
+
+				name = "";
+				
+				System.out.println("이름을 입력하세요");
+				name = scanner.next();
+				
+				for(int i=0; i < subjects.length; i++) {
+					System.out.printf("%s 점수를 입력하세요\n",subjects[i]);
+					scores[i] = scanner.nextInt();
+				}
+				result = student.getPassOrFail(name, subjects, scores);
+				System.out.println(result);
+				
+			break;
+			case 8:
+				double[] records = new double[3];
+				System.out.println("기록측정");
+				System.out.print("시간입력>");
+				for(int i = 0; i < 3; i++) {
+					records[i] = scanner.nextDouble();
+				}
+				result = student.getRank(records);
+				
+				System.out.println(result);
+
+			break;
+			case 9:
+				int count = 0;
+				int[] numbers = null;
+				while(true) {
+					
+					System.out.println("숫자입력 (-1종료)");
+					int sc = scanner.nextInt();
+					if(sc == -1) {
+						break;
+					}
+					int[] temp = new int[count];
+					for(int i = 0; i < count ; i++) {
+						temp[i] = numbers[i];
+						
+					}
+					count ++;
+					numbers = new int[count];
+					for(int i = 0 ;i <count-1 ; i++) {
+						numbers[i] = temp[i];
+						
+					}
+					numbers[count-1] = sc;
+					
+					
+				}
+				result = student.getScoreCalc(numbers);
 				
 				
 				
+				System.out.println(result);
+			
+			case 10:
+				System.out.println("세금계산기");
+				System.out.println("이름>");
+				name = scanner.next();
+				System.out.println("연봉>");
+				int income = scanner.nextInt();
+				result = student.getTax(name, income);
+				System.out.println(result);
+			break;
+			
+			case 11:
+				System.out.println("시간계산기");
+				System.out.println("시간입력:");
+				int time = scanner.nextInt();
+				result = student.getTimeCalc(time);
 				
+				System.out.println(result);
+			
+			break;
 				
 			}
 			
